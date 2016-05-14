abc8f1a74bd4e8f1a14a1b1ef09daf08d326b3ce
Hendrik <hendrik.wels@web.de>
2016-05-14 18:39:07 +0200

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,56 @@
+
+
+// function sayRandomNumber (lower, upper) {
+// 	return randomNumber = Math.floor( Math.random()  * (upper - lower + 1)) + lower;
+
+// }
+
+
+// document.write(sayRandomNumber(10, 20) );
+
+
+// A little quiz about me 
+
+
+// var collect = 0;
+
+// var checkRight = false;
+
+// function questionsRight(collect) {
+// 	if (checkRight === true) {
+// 		collect += 1;
+// 	}
+// 	return collect;
+// }
+
+// document.write(questionsRight(collect));
+
+
+// var question1 = prompt("What job does Hendrik have?");
+// 	if(question1.toUpperCase() === "WEB DESIGNER") {
+// 		document.write("<p>You're right!</p>");
+// 		checkRight === true;
+// 	}
+// 	else{
+// 		document.write("<p>No Hendrik is Web Designer!</p>");
+// 	}
+
+
+// var question2 = prompt("What is Hendrik passionated about?");
+// 	if(question2.toUpperCase() === "WEB DESIGN") {
+// 			document.write("<p>Very good!</p>");
+// 			checkRight === true;
+// 		}
+// 		else{
+// 			document.write("<p>No Hendrik loves Web Design</p>");
+// 		}
+
+
+// var question3 = prompt("How can you reach out to Hendrik?");
+// 	if(question3.toUpperCase() === "FACEBOOK" || question3.toUpperCase() === "MAIL"  ) {
+// 			document.write("<p>Right you can contact Hendrik via Mail or facebook</p>");
+// 			checkRight === true;
+// 		}
+// 		else{
+// 			document.write("<p>No you can contact Hendrik via Mail or Facebook</p>");
+// 		}
